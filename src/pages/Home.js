1e84d98fa4b87b3680b8ee076dc3e23f2130eb5e
kimlie0401 <54561489+kimlie0401@users.noreply.github.com>
2020-02-12 17:56:44 -0800

    #3 Displaying Posts

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,41 @@
-import React, { Component } from "react";
+import React from "react";
+import { useQuery } from "@apollo/react-hooks";
+import gql from "graphql-tag";
 
-export class Home extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Home Page</h1>
-      </div>
-    );
+const FETCH_POSTS_QUERY = gql`
+  {
+    getPosts {
+      id
+      body
+      createdAt
+      username
+      likeCount
+      likes {
+        username
+      }
+      commentCount
+      comments {
+        id
+        username
+        createdAt
+        body
+      }
+    }
   }
-}
+`;
+
+const Home = props => {
+  const { loading, error, data } = useQuery(FETCH_POSTS_QUERY);
+
+  if (data) {
+    console.log(data);
+  }
+
+  return (
+    <div>
+      <h1>Home Page</h1>
+    </div>
+  );
+};
 
 export default Home;
