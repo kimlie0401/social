687ddbcaf9cb9f3508f505f3e4d154f455b5d84d
kimlie0401 <54561489+kimlie0401@users.noreply.github.com>
2020-02-22 18:25:44 -0800

    #9 Displaying, Adding & Deleting Comments

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -25,30 +25,30 @@ const Home = props => {
   // }
 
   return (
-    <Grid columns={3} stackable>
-      <Grid.Row className="page-title">
-        <h1>Recent Posts</h1>
-      </Grid.Row>
-      <Grid.Row>
-        {user && (
-          <Grid.Column>
-            <PostForm />
-          </Grid.Column>
-        )}
-        {loading ? (
-          <Loader />
-        ) : (
-          <Transition.Group>
-            {posts &&
-              posts.map(post => (
-                <Grid.Column key={post.id} style={{ marginBottom: 20 }}>
-                  <PostCard post={post} />
-                </Grid.Column>
-              ))}
-          </Transition.Group>
-        )}
-      </Grid.Row>
-    </Grid>
+    <>
+      <h1 className="page-title">Recent Posts</h1>
+      <Grid columns={3} stackable>
+        <Grid.Row>
+          {user && (
+            <Grid.Column>
+              <PostForm />
+            </Grid.Column>
+          )}
+          {loading ? (
+            <Loader />
+          ) : (
+            <Transition.Group>
+              {posts &&
+                posts.map(post => (
+                  <Grid.Column key={post.id} style={{ marginBottom: 20 }}>
+                    <PostCard post={post} />
+                  </Grid.Column>
+                ))}
+            </Transition.Group>
+          )}
+        </Grid.Row>
+      </Grid>
+    </>
   );
 };
 
